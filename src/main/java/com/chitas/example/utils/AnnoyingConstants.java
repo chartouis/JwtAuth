da53f07c1c6e07357bb4e4477aeb93eef2ae89a8
Chitos <erna95er@mail.ru>
2025-03-27 23:15:47 +0500

    annoying constants

---
+++ src/main/java/com/chitas/example/utils/AnnoyingConstants.java
@@ -0,0 +1,27 @@
+package utils;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import com.chitas.example.model.User;
+import com.chitas.example.repo.UsersRepo;
+
+
+@Component
+public class AnnoyingConstants {
+    private final UsersRepo usersRepo;
+
+    public AnnoyingConstants(UsersRepo usersRepo){
+        this.usersRepo = usersRepo;
+
+    }
+
+    public User getCurrentUser(){
+        return usersRepo.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+    }
+
+    public String getCurrentUsername(){
+        return getCurrentUser().getUsername();
+    }
+
+}
